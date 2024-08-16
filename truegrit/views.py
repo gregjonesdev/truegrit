eec4ce806593f908ee70ef5c39468b5a35d391be
gregjones <gregjones@algorist.io>
2024-08-16 18:43:44 -0500

    create first template

--- truegrit/views.py
+++ truegrit/views.py
@@ -2,11 +2,10 @@ from django.views.generic import View
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class FrontPage(LoginRequiredMixin, View):
+class FrontPage(View):
 
     template_name = 'index.html'
     context = {}
 
     def get(self, request, *args, **kwargs):
-        
         return render(request, self.template_name, self.context)
