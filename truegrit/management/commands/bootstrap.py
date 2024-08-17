49bb3b527156d865606fa3bfb8961d84233b59ae
gregjones <gregjones@algorist.io>
2024-08-17 00:26:46 -0500

    create method to seed project status

--- truegrit/management/commands/bootstrap.py
+++ truegrit/management/commands/bootstrap.py
@@ -6,5 +6,11 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):  
 
+    def seed_projectstatus(self, project_status):
+        print("Seeding project status..")
+        for status in project_status:
+            print(status["name"])
+
     def handle(self, *args, **options):
-        print("hello")
+        jsonData = json.loads(open('./truegrit/json/data.json').read())
+        self.seed_projectstatus(jsonData["project_status"])
