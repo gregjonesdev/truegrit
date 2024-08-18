da8ddbf74177b34a6c565784afcb85c37ef9168e
gregjones <gregjones@algorist.io>
2024-08-17 20:49:54 -0500

    add fields and camera models

--- truegrit/management/commands/dummydata.py
+++ truegrit/management/commands/dummydata.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def create_cameras(self):
-        for i in range(50):
+        for i in range(15):
             print(i)
 
     def handle(self, *args, **options):
--- truegrit/models.py
+++ truegrit/models.py
@@ -35,3 +35,36 @@ class ServerManufacturer(Manufacturer):
 class CameraManufacturer(Manufacturer):
 
     pass
+
+class CameraModel(CoreModel):
+
+    manufacturer = models.ForeignKey(
+        CameraManufacturer,
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(
+        max_length=64,
+    )
+
+class Camera(CoreModel):
+
+    model = models.ForeignKey(
+        CameraModel,
+        on_delete=models.CASCADE
+    )
+
+    mac_address = models.CharField(
+        max_length=12,
+    )
+
+class CameraInstallation(CoreModel):
+
+    camera = models.ForeignKey(
+        Camera,
+        on_delete=models.CASCADE
+    )    
+
+    # ip_address
+
+    # installation mount type
