1c6a459626ecbc025fd9251aeddb7b4a3e9075d2
Gregory Jones <GregoryJones@pref-tech.com>
2024-10-25 11:21:52 -0500

    working on static

--- truegrit/management/commands/savereport.py
+++ truegrit/management/commands/savereport.py
@@ -13,6 +13,9 @@ from truegrit.models import Network, Camera
 username = 'root'
 password = 'h3bc4m3r4'
 
+GREEN = '\033[92m'
+RESET = '\033[0m'
+
 # Define file paths
 csv_file_path = r"C:\Users\gregoryjones\OneDrive - Preferred Technologies, LLC\Documents\My Data Sources\Devices.csv"
 excel_file_path = r"C:\Users\gregoryjones\OneDrive - Preferred Technologies, LLC\Book3.xlsx"
@@ -38,7 +41,7 @@ class Command(BaseCommand):
             response = requests.get(url, auth=HTTPDigestAuth(username, password))
             return response.content
         except requests.exceptions.ConnectionError:
-            print("Unable to connect to {}".format(ip_address))
+            print("\nUnable to connect to {}".format(ip_address))
 
     def get_attribute_from_ip(self, ip_address, attribute_name):
         attribute_string = str(self.get_attribute_string(
@@ -85,9 +88,10 @@ class Command(BaseCommand):
             cell_f = excel_row[5]  # Column F is the 6th column in zero-based index
 
             if cell_f.value == ip_address:  # Check if the value in F matches the extracted value
-                # Assign values to columns K and J (10th and 9th columns)
-                excel_row[10].value = row[0]  # Column K mac address
-                excel_row[8].value = row[4]   # Column I firmware
+                # Assign values to columns K and J (10th and 9th columns)    
+                excel_row[8].value = firmware_version   # Column I firmware
+                excel_row[9].value = is_dot1x_disabled
+                excel_row[10].value = mac_address  # Column K mac address
                 break  # Exit after finding the match
         
 
@@ -120,14 +124,36 @@ class Command(BaseCommand):
         camera.mac_address = mac_address 
         camera.save()
 
-        
+    def updateProperty(self, ip_address, property, value):
+        text_string = "Set '{}' to '{}'".format(property.replace("root.",""), value)
+        url = "http://{}/axis-cgi/param.cgi?action=update&{}={}".format(
+            ip_address,
+            property,
+            value
+        )
+        response = requests.get(url, auth=HTTPDigestAuth(username, password))
+        checkmark = '\u2713'
+        if response.status_code == 200:
+            print(f"\t\t{GREEN}{checkmark}{RESET} {text_string}")
+        else:
+            print("Something went wrong...")
+            error_pattern = r'<p>(.*?)</p>'
+            string_response = str(response._content).replace("\\n", " ").replace("\\", "")
+            error_message = re.search(error_pattern, string_response)
+            print(error_message.group(1))
+            raise SystemExit(0)    
 
     def handle(self, *args, **options):
         self.clear_screen() 
         bu_identifier = input("Enter business unit ID: \n")             
         network = self.get_network_from_bu(bu_identifier)
         # Load the workbook and select the specific sheet
-        wb = load_workbook(excel_file_path)
+        try:
+            wb = load_workbook(excel_file_path)
+        except PermissionError:
+            short_file_name = excel_file_path.split("\\")[-1]
+            print("\nPermission Error: Please close {} and try again.\n".format(short_file_name))
+            raise SystemExit(0)
         sheet = wb[bu_identifier]
 
         
@@ -144,20 +170,19 @@ class Command(BaseCommand):
                 firmware_version = row[4]
                 
                 camera = self.get_camera(ip_address, network)
-                print(camera)
-                raise SystemExit(0)
-                self.save_mac_address(camera, mac_address)
-                self.configure_device(ip_address, camera.get_upnp_name()) 
-                is_dot1x_disabled = self.is_dot1x_disabled(ip_address)
-                  
-                # Iterate through rows in Excel sheet to find the match in column F (6th column, 1-indexed)
-                self.update_xls(
-                    sheet, 
-                    ip_address, 
-                    firmware_version, 
-                    is_dot1x_disabled, 
-                    mac_address)
+                if camera:
+                    self.save_mac_address(camera, mac_address)
+                    self.configure_device(ip_address, camera.get_upnp_name()) 
+                    is_dot1x_disabled = self.is_dot1x_disabled(ip_address)
+                    
+                    # Iterate through rows in Excel sheet to find the match in column F (6th column, 1-indexed)
+                    self.update_xls(
+                        sheet, 
+                        ip_address, 
+                        firmware_version, 
+                        is_dot1x_disabled, 
+                        mac_address)
 
         # Save the modified workbook
         wb.save(excel_file_path)
-        print("Excel file updated successfully.")
+        print("\nExcel file updated successfully.")
