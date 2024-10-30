680de24ee7047d74af175264cadbac8117d4eb4f
Gregory Jones <GregoryJones@pref-tech.com>
2024-10-30 12:11:28 -0500

    works

--- truegrit/management/commands/ip_ranges.py
+++ truegrit/management/commands/ip_ranges.py
@@ -2,7 +2,7 @@ import csv
 from django.core.management.base import BaseCommand
 
 from truegrit.models import Camera
-
+from ipaddress import ip_address
 # csv_file_path = r"C:\Users\gregoryjones\OneDrive - Preferred Technologies, LLC\Documents\My Data Sources\Devices.csv"
 
 class Command(BaseCommand):
@@ -11,7 +11,7 @@ class Command(BaseCommand):
         # Sort IPs numerically
         ip_list = sorted(ip_list, key=lambda ip: int(ip_address(ip)))
         
-        ranges = []
+        ranges = ""
         start = ip_list[0]
         end = start
 
@@ -23,22 +23,30 @@ class Command(BaseCommand):
             if int(ip_address(current_ip)) == int(ip_address(previous_ip)) + 1:
                 end = current_ip  # Update the end of the current range
             else:
-                # Append range to the list
+                # Append range to the string without initial comma
+                if ranges:  # Add a comma only if ranges is not empty
+                    ranges += ","
+                    
                 if start == end:
-                    ranges.append(start)
+                    ranges += f"{start}"
                 else:
-                    ranges.append(f"{start}-{end}")
+                    formatted_end = end.split(".")[-1]
+                    ranges += f"{start}-{formatted_end}"
+                    
                 # Start a new range
                 start = current_ip
                 end = current_ip
 
         # Append the final range
+        if ranges:
+            ranges += ","
         if start == end:
-            ranges.append(start)
+            ranges += f"{start}"
         else:
-            ranges.append(f"{start}-{end}")
+            formatted_end = end.split(".")[-1]
+            ranges += f"{start}-{formatted_end}"
 
-        return ranges  
+        return ranges
 
     def get_network_from_bu(self, bu_identifier):
         return Network.objects.get(business_unit__identifier=bu_identifier)    
@@ -56,7 +64,13 @@ class Command(BaseCommand):
             mac_address__isnull=True):
             if not camera.model.name in unassigned_cameras.keys():
                 unassigned_cameras[camera.model.name] = []
+            
+            unassigned_cameras[camera.model.name].append(camera.ip_address)
+        
+        print(unassigned_cameras) 
 
-        print(unassigned_cameras)        
+        print(unassigned_cameras['M4216-V'])      
+
+        print(self.ip_ranges(unassigned_cameras['M4216-V'])) 
 
                                                                                     
